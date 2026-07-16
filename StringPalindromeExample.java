b01272f1637a8ef55f3c732bdbaf1e0ffd4b474a
Sravanthi Mulagalapalli <mulagalapallisravanthi@gmail.com>
2026-07-16 23:43:48 +0530

    Initial commit

---
+++ StringPalindromeExample.java
@@ -0,0 +1,17 @@
+package strings;
+public class StringPalindromeExample {
+	public static void main(String[] args) {
+        String str = "madam";
+        String reversed = "";
+        for (int i = str.length() - 1; i >= 0; i--) {
+            reversed += str.charAt(i);
+        }
+        if (str.equals(reversed)) {
+            System.out.println(str + " is a Palindrome");
+        } else {
+            System.out.println(str + " is not a Palindrome");
+        }
+    }
+
+
+}
